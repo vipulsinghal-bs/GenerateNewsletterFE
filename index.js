286f5194b2f55da170007739e26df30ec2d5e7db
vipulsinghal_bs <vipul.singhal@bigsteptech.com>
2023-09-26 11:48:34 +0530

    Chat scrapper and Newsletter generation Frontend code

---
+++ index.js
@@ -0,0 +1,73 @@
+// const generateNewsletter = require('./newsletter_generator');
+import {hello} from './newsletter_generator.js';
+document.addEventListener("DOMContentLoaded", function () {
+    const apiUrl = 'http://localhost:3000/scrape';
+    const myForm = document.getElementById("myForm");
+
+    myForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        console.log(event)
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const url = document.getElementById("url").value;
+        const duration = document.getElementById("duration").value;
+
+        const requestData = {
+            email: email,
+            password: password,
+            space_url: url,
+            months: duration
+          };
+
+        // Simple email format validation
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!email.match(emailRegex)) {
+            alert("Please enter a valid email address.");
+            return;
+        }
+
+        // Password length validation
+        if (password.length < 6) {
+            alert("Password must be at least 6 characters long.");
+            return;
+        }
+
+        // Additional custom validation for the URL can be added here
+
+        // If all validation passes, log the values
+        console.log("Email:", email);
+        console.log("Password:", password);
+        console.log("URL:", url);
+        console.log("Duration:", duration + " month(s)");
+
+         const requestOptions = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              // You may need to add other headers like authorization tokens here
+            },
+            body: JSON.stringify(requestData)
+          };
+
+          alert("We are processing the messages. Your newsletter will be downloaded after few minutes.")  
+          
+          // Make the POST request
+          fetch(apiUrl, requestOptions)
+          .then(response => {
+              if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+              return response.json();
+            })
+            .then(data => {
+              console.log('API Response:', data.data);
+
+              hello(data.data);
+            })
+            .catch(error => {
+              console.error('Error:', error);
+            });
+
+      
+    });
+});
---
+++ newsletter_generator.js
@@ -0,0 +1,76 @@
+export async function hello(data) {
+    const outputDir = 'output';
+  
+    // Load newsletter data from JSON
+    // const newsletterDataResponse = await fetch('data/newsletter_data.json');
+    const newsletterDataArray = data;
+  
+   console.log("newsletter_data:",data)
+
+    // Read the content template for the content section only
+    const contentTemplatePath = `./templates/content_template.txt`;
+    const contentTemplateResponse = await fetch(contentTemplatePath);
+    const contentTemplateContent = await contentTemplateResponse.text();
+  
+    // Initialize an empty string to store the combined HTML content
+    let combinedHtml = '';
+  
+    // Iterate through newsletter data objects and generate combined content
+    for (const newsletterData of newsletterDataArray) {
+      // Create a dictionary with data for rendering the content section
+  
+      const data = {
+        text: newsletterData.text,
+        title: newsletterData.title,
+        imgSrc: newsletterData.imgSrc,
+        name: newsletterData.name,
+      };
+  
+      // Render the content section template with data using template literals
+      const renderedContent = `
+        <div class="card">
+          <div class="container">
+          <img src="${data.imgSrc}" alt="Avatar" style="width:400px" class="center-img">
+          <h4><b>${data.name}</b></h4>
+            <p>${data.text}</p>
+          </div>
+        </div>
+        <br>
+      `;
+  
+      // Append the rendered content section to the combinedHtml string
+      combinedHtml += renderedContent;
+    }
+  
+    // Generate a single filename for the combined content
+    const combinedFilename = `${outputDir}/combined_newsletter.html`;
+  
+    // Read the main template for the main HTML structure
+    const mainTemplatePath = `./templates/newsletter_template.txt`;
+    const mainTemplateResponse = await fetch(mainTemplatePath);
+    const mainTemplateContent = await mainTemplateResponse.text();
+  
+    // Replace the title placeholder in the main template with the first title from JSON
+    const firstTitle = newsletterDataArray.length > 0 ? newsletterDataArray[0].title : '';
+    let finalNewsletter = mainTemplateContent.replace('<%= title %>', firstTitle);
+  
+    // Replace the content placeholder in the main template with the combined content
+    finalNewsletter = finalNewsletter.replace('<%- content_placeholder %>', combinedHtml);
+  
+    // Create a Blob containing the HTML content
+    const blob = new Blob([finalNewsletter], { type: 'text/html' });
+  
+    // Create an object URL for the Blob
+    const blobUrl = URL.createObjectURL(blob);
+  
+    // Create a link element for downloading the file
+    const a = document.createElement('a');
+    a.href = blobUrl;
+    a.download = 'combined_newsletter.html';
+  
+    // Trigger a click event to download the file
+    a.click();
+  
+    console.log('Combined newsletter generated successfully!');
+  }
+  
